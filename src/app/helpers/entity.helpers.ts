5f4c7621b0c50390fa3e60bfc6c7dea8fd571a8c
Igor Milly <igor.milly@protonmail.com>
2023-04-16 11:28:48 +0200

    fix: misc import

--- src/app/helpers/entity.helpers.ts
+++ src/app/helpers/entity.helpers.ts
@@ -1,11 +1,5 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { EntityActionDataServiceError, EntityActionPayload } from '@ngrx/data';
 import { Dictionary } from '@ngrx/entity';
 
 export function getEntityById<T>([entityMap, id]: [Dictionary<T>, string | number | undefined]): T | undefined {
   return id ? entityMap[id] : undefined;
-}
-
-export function getEntityPayloadError(payload: EntityActionPayload<EntityActionDataServiceError>): Partial<HttpErrorResponse> | undefined {
-  return payload.data?.error.error;
 }
