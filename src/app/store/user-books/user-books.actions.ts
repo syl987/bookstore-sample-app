1e0568332060246d68f43a034fd723978a504090
Igor Milly <igor.milly@protonmail.com>
2023-04-19 11:30:22 +0200

    feat: actions

--- src/app/store/user-books/user-books.actions.ts
+++ src/app/store/user-books/user-books.actions.ts
@@ -3,28 +3,28 @@ import { UserBookDTO, UserBookEditDraftDTO } from 'src/app/models/book.models';
 import { ResponseError } from 'src/app/models/error.models';
 import { GoogleBooksVolumeDTO } from 'src/app/models/google-books.models';
 
-export const loadUserBook = createAction('[UserBooks] load', props<{ cid: number; id: string }>());
-export const loadUserBookSuccess = createAction('[UserBooks] load SUCCESS', props<{ cid: number; book: UserBookDTO }>());
-export const loadUserBookError = createAction('[UserBooks] load ERROR', props<{ cid: number; error: ResponseError }>());
+export const loadUserBook = createAction('[UserBooks] load', props<{ id: string }>());
+export const loadUserBookSuccess = createAction('[UserBooks] load SUCCESS', props<{ book: UserBookDTO }>());
+export const loadUserBookError = createAction('[UserBooks] load ERROR', props<{ error: ResponseError }>());
 
-export const loadUserBooks = createAction('[UserBooks] load all', props<{ cid: number }>());
-export const loadUserBooksSuccess = createAction('[UserBooks] load all SUCCESS', props<{ cid: number; books: UserBookDTO[] }>());
-export const loadUserBooksError = createAction('[UserBooks] load all ERROR', props<{ cid: number; error: ResponseError }>());
+export const loadUserBooks = createAction('[UserBooks] load all');
+export const loadUserBooksSuccess = createAction('[UserBooks] load all SUCCESS', props<{ books: UserBookDTO[] }>());
+export const loadUserBooksError = createAction('[UserBooks] load all ERROR', props<{ error: ResponseError }>());
 
-export const createUserBook = createAction('[UserBooks] create', props<{ cid: number; volumeData: GoogleBooksVolumeDTO }>());
-export const createUserBookSuccess = createAction('[UserBooks] create SUCCESS', props<{ cid: number; book: UserBookDTO }>());
-export const createUserBookError = createAction('[UserBooks] create ERROR', props<{ cid: number; error: ResponseError }>());
+export const createUserBook = createAction('[UserBooks] create', props<{ volumeData: GoogleBooksVolumeDTO }>());
+export const createUserBookSuccess = createAction('[UserBooks] create SUCCESS', props<{ book: UserBookDTO }>());
+export const createUserBookError = createAction('[UserBooks] create ERROR', props<{ error: ResponseError }>());
 
-export const deleteUserBook = createAction('[UserBooks] delete', props<{ cid: number; id: string }>());
-export const deleteUserBookSuccess = createAction('[UserBooks] delete SUCCESS', props<{ cid: number; id: string }>());
-export const deleteUserBookError = createAction('[UserBooks] delete ERROR', props<{ cid: number; error: ResponseError }>());
+export const deleteUserBook = createAction('[UserBooks] delete', props<{ id: string }>());
+export const deleteUserBookSuccess = createAction('[UserBooks] delete SUCCESS', props<{ id: string }>());
+export const deleteUserBookError = createAction('[UserBooks] delete ERROR', props<{ error: ResponseError }>());
 
-export const editUserBookDraft = createAction('[UserBooks] edit draft', props<{ cid: number; id: string; data: UserBookEditDraftDTO }>());
-export const editUserBookDraftSuccess = createAction('[UserBooks] edit draft SUCCESS', props<{ cid: number; book: UserBookDTO }>());
-export const editUserBookDraftError = createAction('[UserBooks] edit draft ERROR', props<{ cid: number; error: ResponseError }>());
+export const editUserBookDraft = createAction('[UserBooks] edit draft', props<{ id: string; data: UserBookEditDraftDTO }>());
+export const editUserBookDraftSuccess = createAction('[UserBooks] edit draft SUCCESS', props<{ book: UserBookDTO }>());
+export const editUserBookDraftError = createAction('[UserBooks] edit draft ERROR', props<{ error: ResponseError }>());
 
-export const publishUserBook = createAction('[UserBooks] publish', props<{ cid: number; id: string }>());
-export const publishUserBookSuccess = createAction('[UserBooks] publish SUCCESS', props<{ cid: number; book: UserBookDTO }>());
-export const publishUserBookError = createAction('[UserBooks] publish ERROR', props<{ cid: number; error: ResponseError }>());
+export const publishUserBook = createAction('[UserBooks] publish', props<{ id: string }>());
+export const publishUserBookSuccess = createAction('[UserBooks] publish SUCCESS', props<{ book: UserBookDTO }>());
+export const publishUserBookError = createAction('[UserBooks] publish ERROR', props<{ error: ResponseError }>());
 
 // TODO buy actions
