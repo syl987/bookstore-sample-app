0fe2156fda60dfd5d29c076cc675473cc6850068
Igor Milly <igor.milly@protonmail.com>
2023-04-20 22:14:59 +0200

    feat: book create selection list

--- src/app/components/user-book-create-dialog/user-book-create-dialog.component.ts
+++ src/app/components/user-book-create-dialog/user-book-create-dialog.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSelectionList } from '@angular/material/list';
+import { Subject, takeUntil } from 'rxjs';
 import { UserBookDTO } from 'src/app/models/book.models';
 import { GoogleBooksVolumeDTO } from 'src/app/models/google-books.models';
 import { GoogleBooksService } from 'src/app/services/google-books.service';
@@ -14,7 +16,7 @@ import { UserBooksService } from 'src/app/services/user-books.service';
   templateUrl: './user-book-create-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserBookCreateDialogComponent {
+export class UserBookCreateDialogComponent implements AfterViewInit, OnDestroy {
   readonly volumes$ = this.googleBooksService.searchVolumes$;
 
   readonly searchPending$ = this.googleBooksService.searchPending$;
@@ -22,11 +24,25 @@ export class UserBookCreateDialogComponent {
 
   readonly creating$ = this.userBooksService.creating$;
 
+  @ViewChild(MatSelectionList) list?: MatSelectionList;
+
+  private readonly _destroyed$ = new Subject<void>();
+
   constructor(
     readonly dialogRef: MatDialogRef<UserBookCreateDialogComponent, UserBookDTO | undefined>,
     private readonly userBooksService: UserBooksService,
     private readonly googleBooksService: GoogleBooksService,
+    private readonly detector: ChangeDetectorRef,
   ) {}
+
+  ngAfterViewInit(): void {
+    this.list!.selectedOptions.changed.pipe(takeUntil(this._destroyed$)).subscribe(_ => this.detector.markForCheck());
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
 
   searchVolumes(query: string | null): void {
     this.googleBooksService.searchVolumes(query ?? '');
