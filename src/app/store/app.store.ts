3fffc88e4a7112c69232a04c324dceee6a0eb408
Igor Milly <igor.milly@protonmail.com>
2023-04-26 18:31:26 +0200

    feat: connect search store

--- src/app/store/app.store.ts
+++ src/app/store/app.store.ts
@@ -6,6 +6,8 @@ import { metaReducers } from './app.meta-reducers';
 import { AuthEffects } from './auth/auth.effects';
 import { GoogleBooksEffects } from './google-books/google-books.effects';
 import * as fromGoogleBooks from './google-books/google-books.reducer';
+import { SearchEffects } from './search/search.effects';
+import * as fromSearch from './search/search.reducer';
 import { UserBooksEffects } from './user-books/user-books.effects';
 import * as fromUserBooks from './user-books/user-books.reducer';
 import { VolumesEffects } from './volume/volume.effects';
@@ -13,6 +15,7 @@ import * as fromVolume from './volume/volume.reducer';
 
 interface AppState {
   router: RouterReducerState<MinimalRouterStateSnapshot>;
+  [fromSearch.searchFeatureKey]: fromSearch.State;
   [fromGoogleBooks.googleBooksFeatureKey]: fromGoogleBooks.State;
   [fromUserBooks.userBooksFeatureKey]: fromUserBooks.State;
   [fromVolume.volumeFeatureKey]: fromVolume.State;
@@ -20,12 +23,13 @@ interface AppState {
 
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
+  [fromSearch.searchFeatureKey]: fromSearch.reducer,
   [fromGoogleBooks.googleBooksFeatureKey]: fromGoogleBooks.reducer,
   [fromUserBooks.userBooksFeatureKey]: fromUserBooks.reducer,
   [fromVolume.volumeFeatureKey]: fromVolume.reducer,
 };
 
-export const effects = [AuthEffects, GoogleBooksEffects, UserBooksEffects, VolumesEffects];
+export const effects = [AuthEffects, SearchEffects, GoogleBooksEffects, UserBooksEffects, VolumesEffects];
 
 export const storeConfig: RootStoreConfig<AppState> = {
   metaReducers,
