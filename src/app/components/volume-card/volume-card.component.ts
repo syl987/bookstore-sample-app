b8ca3a5014d2900800b6faeb80ac125f06d20623
Igor Milly <53560066+syl987@users.noreply.github.com>
2023-05-07 14:43:09 +0200

    refactor: standalone architecture (#13)
    Co-authored-by: Igor Milly <igor.milly@protonmail.com>

--- src/app/components/volume-card/volume-card.component.ts
+++ src/app/components/volume-card/volume-card.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { VolumeDTO } from 'src/app/models/volume.models';
 
 // TODO loading appearence
 
 @Component({
   selector: 'app-volume-card',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatDividerModule],
   templateUrl: './volume-card.component.html',
   styles: ['.mat-mdc-card-sm-image { width: inherit; }'],
   changeDetection: ChangeDetectionStrategy.OnPush,
