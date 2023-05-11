44eba5c730f3078a6ea08f1cdbd6caffd76e42e8
Igor Milly <igor.milly@protonmail.com>
2023-05-11 21:42:51 +0200

    feat: title bar progress

--- src/app/components/__base/title-bar/title-bar.component.ts
+++ src/app/components/__base/title-bar/title-bar.component.ts
@@ -1,18 +1,22 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { RouterService } from 'src/app/services/router.service';
 
 @Component({
   selector: 'app-title-bar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './title-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'd-flex flex-wrap app-gap-x-2xs' },
 })
 export class TitleBarComponent {
   readonly title$ = this.routerService.title$;
 
-  constructor(private readonly observer: BreakpointObserver, private readonly routerService: RouterService) {}
+  @Input() backUrl?: string | null;
+
+  constructor(private readonly routerService: RouterService) {}
 }
