c7ccf110e2789e82378fe7f5d7daf25bdb865c3f
Igor Milly <igor.milly@protonmail.com>
2023-05-11 13:59:40 +0200

    feat: simplify header

--- src/app/components/__base/header/header.component.ts
+++ src/app/components/__base/header/header.component.ts
@@ -34,10 +34,6 @@ export class HeaderComponent {
 
   readonly displayLogin$ = this.routerService.url$.pipe(map(url => !url?.startsWith('/login')));
 
-  readonly title$ = this.routerService.title$;
-
-  readonly toolbarTitle$ = combineLatest([this.desktop$, this.title$]).pipe(map(([desktop, title]) => (desktop ? this.options.applicationName : title)));
-
   readonly LINKS = APP_NAV_LINKS.filter(link => !link.dev || isDevMode());
   readonly PUBLIC_LINKS = this.LINKS.filter(link => !link.user);
   readonly USER_LINKS = this.LINKS.filter(link => link.user);
