49f42c3fe484007bbe5af051715407f79353eb1f
Igor Milly <igor.milly@protonmail.com>
2023-05-17 13:00:10 +0200

    feat: revert sidenav

--- src/app/components/__base/sidenav/sidenav.component.ts
+++ src/app/components/__base/sidenav/sidenav.component.ts
@@ -2,9 +2,8 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, isDevMode, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import { Router, RouterModule } from '@angular/router';
-import { APP_LANGUAGES, APP_NAV_LINKS } from 'src/app/models/app.models';
+import { APP_NAV_LINKS } from 'src/app/models/app.models';
 import { AuthUser } from 'src/app/models/auth.models';
 import { AuthService } from 'src/app/services/auth.service';
 import { DialogService } from 'src/app/services/dialog.service';
@@ -14,7 +13,7 @@ import { DialogService } from 'src/app/services/dialog.service';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule, MatListModule, MatMenuModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatListModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,8 +23,6 @@ export class SidenavComponent {
 
   readonly LINKS = APP_NAV_LINKS.filter(link => !link.dev || isDevMode());
   readonly PUBLIC_LINKS = this.LINKS.filter(link => !link.user);
-
-  readonly APP_LANGUAGES = APP_LANGUAGES;
 
   @Output() readonly navigated = new EventEmitter<void>();
 
