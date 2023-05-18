39d326c6c7e75737857174a688942c8fbcd7569d
Igor Milly <igor.milly@protonmail.com>
2023-05-18 21:30:58 +0200

    feat: fix dialog focus

--- src/app/options/dialog.options.ts
+++ src/app/options/dialog.options.ts
@@ -1,7 +1,7 @@
 import { MatDialogConfig } from '@angular/material/dialog';
 
 export const dialogOptions: MatDialogConfig = {
-  autoFocus: true,
+  autoFocus: 'dialog',
   hasBackdrop: true,
   restoreFocus: true,
   width: '100%',
