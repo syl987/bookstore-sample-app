f49cf86a51ea68cabb4051dd099a5129e488280c
Igor Milly <igor.milly@protonmail.com>
2023-05-31 08:58:52 +0200

    feat: storage service progress

--- src/app/services/__api/firebase-storage.service.ts
+++ src/app/services/__api/firebase-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { deleteObject, ref, Storage, uploadBytesResumable } from '@angular/fire/storage';
-import { from, Observable, tap } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { toFirebaseUploadData } from 'src/app/helpers/firebase.helpers';
+import { FirebaseUploadData, FirebaseUploadRequestMetadata } from 'src/app/models/firebase.models';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +10,21 @@ import { from, Observable, tap } from 'rxjs';
 export class FirebaseStorageService {
   constructor(private readonly storage: Storage) {}
 
-  upload(path: string, data: any, options: { contentType?: string; contentEncoding?: string } = {}): Observable<string> {
-    const task = uploadBytesResumable(ref(this.storage, path), data, options);
-
-    return from(task.then(snapshot => snapshot.ref.fullPath) as Promise<string>);
+  uploadUserBookImage(uid: string, id: string, data: Blob | Uint8Array | ArrayBuffer, options: FirebaseUploadRequestMetadata = {}): Observable<FirebaseUploadData> {
+    const reference = ref(this.storage, `userBooks/${uid}/${id}`);
+    const task = uploadBytesResumable(reference, data, options);
+    return new Observable(subscriber => {
+      task.on('state_changed', {
+        next: snapshot => subscriber.next(toFirebaseUploadData(snapshot)),
+        error: subscriber.error,
+        complete: subscriber.complete,
+      });
+    });
   }
 
-  delete(path: string): Observable<void> {
-    const task = deleteObject(ref(this.storage, path));
-
-    return from(task).pipe(tap(console.log)); // TODO kick console
+  deleteUserBookImage(uid: string, id: string): Observable<void> {
+    const reference = ref(this.storage, `userBooks/${uid}/${id}`);
+    const task = deleteObject(reference);
+    return from(task);
   }
 }
