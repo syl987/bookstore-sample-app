5f4fe1694e4073fd3384f223fef179ed1714a8d7
Igor Milly <igor.milly@protonmail.com>
2023-09-17 14:17:21 +0200

    feat: empty component

---
+++ src/app/components/volume-offer-detail-page/volume-offer-detail-page.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map } from 'rxjs';
+import { BookDTO } from 'src/app/models/book.models';
+import { VolumeDTO } from 'src/app/models/volume.models';
+import { BookConditionPipe } from 'src/app/pipes/book-condition.pipe';
+import { DialogService } from 'src/app/services/dialog.service';
+import { RouterService } from 'src/app/services/router.service';
+import { VolumeService } from 'src/app/services/volume.service';
+
+import { TitleBarComponent } from '../__base/title-bar/title-bar.component';
+import { VolumeCardComponent } from '../volume-card/volume-card.component';
+
+function getBookOfferById(arg: [VolumeDTO | undefined, string | undefined]): BookDTO | undefined {
+  const [volume, offerId] = arg;
+  return volume && volume.publishedBooks && offerId ? volume.publishedBooks[offerId] : undefined;
+}
+
+@Component({
+  selector: 'app-volume-offer-detail-page',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, VolumeCardComponent, BookConditionPipe, TitleBarComponent],
+  templateUrl: './volume-offer-detail-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VolumeOfferDetailPageComponent {
+  id: string = this.route.snapshot.params['volumeId'];
+
+  readonly volume$ = this.volumeService.entitiyByRoute$;
+  readonly offer$ = combineLatest([this.volume$, this.routerService.selectRouteParam('offerId')]).pipe(map(getBookOfferById));
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly routerService: RouterService,
+    private readonly volumeService: VolumeService,
+    private readonly dialogService: DialogService,
+  ) {
+    this.routerService
+      .selectRouteParam('volumeId')
+      .pipe(takeUntilDestroyed())
+      .subscribe(id => {
+        if (id) {
+          this.id = id;
+          this.volumeService.load(id);
+        }
+      });
+  }
+
+  buyOffer(offer: BookDTO): void {
+    throw new Error('Method not implemented.');
+  }
+}
