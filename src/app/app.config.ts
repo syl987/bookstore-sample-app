0cad896eb1590baae54fde05cf5dd45641a454b4
Igor Milly <igor.milly@protonmail.com>
2023-09-20 10:36:40 +0200

    feat: change after logout url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -64,7 +64,7 @@ const appOptions: AppOptions = {
 const authConfig: AuthConfig = {
   loginUrl: '/login',
   afterLoginUrl: '/',
-  afterLogoutUrl: '/login',
+  afterLogoutUrl: '/home',
   bearerExcluded: [{ url: 'https://www.googleapis.com/books/v1/volumes' }],
   bearerIncluded: [],
   messages: {
