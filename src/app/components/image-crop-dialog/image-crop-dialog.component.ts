034b3c31c98a24726f42a2b3cad3aa7f890c69d8
Igor Milly <igor.milly@protonmail.com>
2023-11-06 11:49:37 +0100

    feat: kick base64 converter

--- src/app/components/image-crop-dialog/image-crop-dialog.component.ts
+++ src/app/components/image-crop-dialog/image-crop-dialog.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { base64ToBlob } from 'base64-blob';
 import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
 
 export interface ImageCropDialogData {
@@ -26,6 +25,6 @@ export class ImageCropDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) private readonly data: ImageCropDialogData) {}
 
   async setResult(event: ImageCroppedEvent): Promise<void> {
-    this.result = event.base64 ? await base64ToBlob(event.base64) : undefined;
+    this.result = event.blob ?? undefined; // TODO remove null conditional
   }
 }
