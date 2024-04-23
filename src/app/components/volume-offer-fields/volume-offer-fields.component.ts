0aa12fc6333846635c5ad3e588f9107763d20f24
Igor Milly <igor.milly@protonmail.com>
2024-04-23 12:39:46 +0200

    feat: volume fields

--- src/app/components/volume-offer-fields/volume-offer-fields.component.ts
+++ src/app/components/volume-offer-fields/volume-offer-fields.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { BookDTO } from 'src/app/models/book.models';
 import { ImageDTO } from 'src/app/models/image.models';
@@ -15,7 +15,7 @@ import { VolumeCardComponent } from '../volume-card/volume-card.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VolumeOfferFieldsComponent {
-  @Input({ required: true }) offer?: BookDTO | null;
+  readonly offer = input<BookDTO | null>();
 
   getOfferPhotos(offer: BookDTO): ImageDTO[] {
     return Object.values(offer.photos ?? {});
