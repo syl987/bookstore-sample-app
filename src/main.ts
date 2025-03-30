e1b864d751605561edc59be9902dc7e559c4a2a4
Igor Milly <igor.milly@protonmail.com>
2025-03-30 14:58:49 +0200

    chore: misc eslint exception

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  // eslint-disable-next-line no-console
+  .catch(err => console.error(err));
