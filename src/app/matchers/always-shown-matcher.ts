2eb3e692b1cbd26ed5c3a8340cda38bd529b0fc6
Igor Milly <igor.milly@protonmail.com>
2025-04-07 01:15:47 +0200

    refactor: simplify error-state-matchers

--- src/app/matchers/always-shown-matcher.ts
+++ src/app/matchers/always-shown-matcher.ts
@@ -1,8 +1,7 @@
-import { AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
 export class AlwaysShownMatcher implements ErrorStateMatcher {
-  isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(): boolean {
     return true;
   }
 }
--- src/app/matchers/dirty-or-touched-matcher.ts
+++ src/app/matchers/dirty-or-touched-matcher.ts
@@ -1,8 +1,8 @@
-import { AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { AbstractControl, FormGroupDirective } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
 export class DirtyOrTouchedMatcher implements ErrorStateMatcher {
-  isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(control: AbstractControl | null, form: FormGroupDirective | null): boolean {
     return !!(control?.invalid && (control.dirty || control.touched || form?.submitted));
   }
 }
