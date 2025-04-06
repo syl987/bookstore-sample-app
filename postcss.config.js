27cbbb356287cc044c2054d2b0d00021c77d0d67
Igor Milly <igor.milly@protonmail.com>
2025-04-06 13:12:35 +0200

    migrate config

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @see https://github.com/postcss/postcss#usage
+ *
+ * @type {import("postcss-load-config").Config}
+ */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
+
+module.exports = config;
