7c7a75dd0e69eeaf479842babb8df293dbf6d5dd
Igor Milly <igor.milly@protonmail.com>
2025-04-16 21:03:09 +0200

    feat(login-page): improve visuals

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ProviderId } from 'firebase/auth';
 
 import { ButtonSpinnerDirective } from 'src/app/directives/button-spinner.directive';
@@ -8,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [MatButtonModule, ButtonSpinnerDirective],
+  imports: [MatButtonModule, MatIconModule, ButtonSpinnerDirective],
   templateUrl: './login-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'grow flex flex-col justify-center' },
