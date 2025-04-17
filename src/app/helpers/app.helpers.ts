cc216df0c2644033b7083f431454505cbb4dba92
Igor Milly <igor.milly@protonmail.com>
2025-04-17 02:46:33 +0200

    feat: adjust helper jsdoc

--- src/app/helpers/app.helpers.ts
+++ src/app/helpers/app.helpers.ts
@@ -3,10 +3,10 @@ import { AppLanguage, AppLanguages } from '../models/app.models';
 /**
  * Get current language by locale.
  *
- * @param languages Available languages.
- * @param locale Current locale id.
- * @returns `AppLanguage` with matching locale id.
- * @throws If non has been matched.
+ * @param languages - Available languages.
+ * @param locale - Current locale id.
+ * @returns A language with matching locale id.
+ * @throws If none has been matched.
  */
 export function getCurrentAppLanguage(languages: AppLanguages, locale: string): AppLanguage {
   const lang = languages.find(l => l.locale === locale);
