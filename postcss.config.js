9d5149021cf2db3e8b539165589063d470cdff37
syl987 <igor.milly@protonmail.com>
2025-05-29 21:34:55 +0200

    chore: align postcss config

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @see https://github.com/postcss/postcss-load-config/blob/main/README.md
+ *
+ * @type {import('postcss-load-config').Config}
+ */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
+
+module.exports = config;
