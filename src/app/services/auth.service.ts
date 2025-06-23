6a6990fe04aa9a4eeeb9a3a3052fc9da91da2a04
syl987 <igor.milly@protonmail.com>
2025-06-23 17:02:36 +0200

    refactor(auth): kick unnecessary service subscriptions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { Auth, user } from '@angular/fire/auth';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { BehaviorSubject, concatMap, map, Observable, of, shareReplay, take, throwError } from 'rxjs';
+import { concatMap, map, Observable, of, shareReplay, take, throwError } from 'rxjs';
 
 import { toAuthUser } from '../helpers/auth.helpers';
 import { AuthProviderId } from '../models/auth.models';
@@ -21,23 +21,21 @@ export class AuthService {
   readonly user = toSignal(user(this.auth).pipe(map(toAuthUser)));
   readonly uid = computed(() => this.user()?.uid);
 
-  private readonly _loginPending = new BehaviorSubject<boolean>(false);
-  readonly loginPending = toSignal(this._loginPending.asObservable(), { requireSync: true });
+  readonly loginPending = toSignal(
+    this.actions.pipe(
+      ofType(AuthActions.loginWithProvider, AuthActions.loginWithProviderSUCCESS, AuthActions.loginWithProviderERROR, AuthActions.authResetState),
+      map(({ type }) => type === AuthActions.loginWithProvider.type),
+    ),
+    { initialValue: false },
+  );
 
-  private readonly _logoutPending = new BehaviorSubject<boolean>(false);
-  readonly logoutPending = toSignal(this._loginPending.asObservable(), { requireSync: true });
-
-  constructor() {
-    // set loginPending stream
-    this.actions.pipe(ofType(AuthActions.loginWithProvider), takeUntilDestroyed()).subscribe(_ => this._loginPending.next(true));
-    this.actions
-      .pipe(ofType(AuthActions.loginWithProviderSUCCESS, AuthActions.loginWithProviderERROR, AuthActions.authResetState), takeUntilDestroyed())
-      .subscribe(_ => this._loginPending.next(false));
-
-    // set logoutPending stream
-    this.actions.pipe(ofType(AuthActions.logout), takeUntilDestroyed()).subscribe(_ => this._logoutPending.next(true));
-    this.actions.pipe(ofType(AuthActions.logoutSUCCESS, AuthActions.logoutERROR, AuthActions.authResetState), takeUntilDestroyed()).subscribe(_ => this._logoutPending.next(false));
-  }
+  readonly logoutPending = toSignal(
+    this.actions.pipe(
+      ofType(AuthActions.logout, AuthActions.logoutSUCCESS, AuthActions.logoutERROR, AuthActions.authResetState),
+      map(({ type }) => type === AuthActions.logout.type),
+    ),
+    { initialValue: false },
+  );
 
   /**
    * Login with an external provider.
