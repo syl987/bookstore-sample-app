5f6e5e40147c8ba7d58fe658bcbe25b6f62b0555
syl987 <igor.milly@protonmail.com>
2025-09-19 11:08:56 +0200

    chore: update stylelint plugin dependency

--- stylelint.config.js
+++ stylelint.config.js
@@ -12,6 +12,7 @@ const config = {
     'stylelint-config-prettier-scss',
   ],
   rules: {
+    'no-invalid-position-declaration': null, // recommended override: support simple media query alterations
     'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['ng-deep'] }], // recommended override: support angular elements
   },
 };
